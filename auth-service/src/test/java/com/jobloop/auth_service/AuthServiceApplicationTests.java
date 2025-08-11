a14ada1584b3b3c77a377b39271f16dd9554062c
ThaisTimoteo <Thaiskarine99@gmail.com>
2025-08-11 14:07:13 -0300

    projeto jobloop

---
+++ auth-service/src/test/java/com/jobloop/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobloop.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
